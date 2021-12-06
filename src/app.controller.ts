8be2dada4293890a2994753f9fda489c652caee6
sandy <braveshining@gmail.com>
2021-12-06 11:59:53 +0800

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Req } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Request } from 'express';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/api/userinfo')
+  getUserInfo(@Req() request: Request): string {
+    // console.log(request);
+    console.log(request);
+
+    return 'userinfo' + request.query;
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('api document')
+    .setDescription('all apis lst')
+    .setVersion('1.0.0')
+    .addTag('nestserver')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8080);
+}
+bootstrap();
